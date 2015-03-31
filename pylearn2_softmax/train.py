df830b5798d56cf9d4fd91c80f0fe27660d0e7cd
szelenin@vagrant <sergey.zelenin@gmail.com>
2015-03-31 21:13:28 +0000

    define algorithm

--- pylearn2_softmax/train.py
+++ pylearn2_softmax/train.py
@@ -6,3 +6,10 @@ with open(os.path.join(dirname, 'sr_dataset.yaml'), 'r') as f:
 hyper_params = {'train_stop' : 50000}
 dataset = dataset % (hyper_params)
 print dataset
+
+with open(os.path.join(dirname, 'sr_algorithm.yaml'), 'r') as f:
+    algorithm = f.read()
+hyper_params = {'batch_size' : 10000,
+                'valid_stop' : 60000}
+algorithm = algorithm % (hyper_params)
+print algorithm
