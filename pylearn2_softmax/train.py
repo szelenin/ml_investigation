a78732cdd7eb56b4c9f55ff778fb0e7ca200b15c
szelenin@vagrant <sergey.zelenin@gmail.com>
2015-04-02 19:16:35 +0000

    forgot to define model

--- pylearn2_softmax/train.py
+++ pylearn2_softmax/train.py
@@ -1,5 +1,5 @@
 import os
-import pylearn2
+
 dirname = os.path.abspath('.')
 with open(os.path.join(dirname, 'sr_dataset.yaml'), 'r') as f:
     dataset = f.read()
@@ -7,9 +7,20 @@ hyper_params = {'train_stop' : 50000}
 dataset = dataset % (hyper_params)
 print dataset
 
+with open(os.path.join(dirname, 'sr_model.yaml'), 'r') as f:
+    model = f.read()
+
+print model
+
 with open(os.path.join(dirname, 'sr_algorithm.yaml'), 'r') as f:
     algorithm = f.read()
 hyper_params = {'batch_size' : 10000,
                 'valid_stop' : 60000}
 algorithm = algorithm % (hyper_params)
 print algorithm
+
+with open(os.path.join(dirname, 'sr_train.yaml'), 'r') as f:
+    train = f.read()
+save_path = '.'
+train = train %locals()
+print train
